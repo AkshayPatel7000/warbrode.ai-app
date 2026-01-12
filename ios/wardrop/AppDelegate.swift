fa70ef28edb353aa0e866b72d588b067efd6e78b
Akshay <akshay.patel@rsvrtech.com>
2026-01-12 13:47:36 +0530

    updated

--- ios/wardrop/AppDelegate.swift
+++ ios/wardrop/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "wardrobeai",
+      withModuleName: "wardrop",
       in: window,
       launchOptions: launchOptions
     )
--- metro.config copy.js
+++ metro.config copy.js
@@ -6,10 +6,10 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const { withNativeWind } = require('nativewind/metro');
-
+const { withNativeWind } = require("nativewind/metro");
+ 
 const config = mergeConfig(getDefaultConfig(__dirname), {
   /* your config */
 });
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: "./global.css" });
